a2b1254f12a8cea184efef5f36ff59ccf8e305c6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2025-03-01 14:57:35 +0400

    feat: setup mongo

---
+++ lib/mongodb.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://root:example@127.0.0.1:27017/blog";
+
+const connectMongo = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      dbName: "blog",
+      authSource: "admin",
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
+
+export default connectMongo; 
