0f5bb5e8da5674bdd5504c983c5a4615b18324b1
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-11-12 00:29:39 +0400

    feat: update packages

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'admin';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const basicAuth = request.headers.get('authorization');
 
   if (basicAuth) {
